371fb4913d58dce9e71f4a5f48901e627608c22b
François Caillet (alias Funflash) <Francois.caillet@gmail.com>
2016-02-01 20:52:33 +0100

    Init projet

---
+++ INFO915_IoT/ObjectCatalog.swift
@@ -0,0 +1,9 @@
+//
+//  ObjectCatalog.swift
+//  INFO915_IoT
+//
+//  Created by François Caillet on 01/02/2016.
+//  Copyright © 2016 François Caillet. All rights reserved.
+//
+
+import Foundation
---
+++ INFO915_IoT/State.swift
@@ -0,0 +1,9 @@
+//
+//  State.swift
+//  INFO915_IoT
+//
+//  Created by François Caillet on 01/02/2016.
+//  Copyright © 2016 François Caillet. All rights reserved.
+//
+
+import Foundation
