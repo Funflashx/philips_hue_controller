ab716ed46717e91019e308674d6a86a41a2bf8f6
François Caillet (alias Funflash) <Francois.caillet@gmail.com>
2016-02-01 16:28:39 +0100

    Initial Commit

---
+++ INFO915_IoT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  INFO915_IoT
+//
+//  Created by François Caillet on 01/02/2016.
+//  Copyright © 2016 François Caillet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ INFO915_IoTTests/INFO915_IoTTests.swift
@@ -0,0 +1,36 @@
+//
+//  INFO915_IoTTests.swift
+//  INFO915_IoTTests
+//
+//  Created by François Caillet on 01/02/2016.
+//  Copyright © 2016 François Caillet. All rights reserved.
+//
+
+import XCTest
+@testable import INFO915_IoT
+
+class INFO915_IoTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
