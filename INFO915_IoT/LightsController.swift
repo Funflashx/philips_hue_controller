1b082656814ea6040a9b07e1a2ec6134d62ad6af
François Caillet (alias Funflash) <Francois.caillet@gmail.com>
2016-02-29 08:57:27 +0100

    list of lights & switch on/off

--- INFO915_IoT/LightsController.swift
+++ INFO915_IoT/LightsController.swift
@@ -1,9 +1,13 @@
 //
-//  ObjectCatalog.swift
+//  LightsController.swift
 //  INFO915_IoT
 //
 //  Created by François Caillet on 15/02/2016.
 //  Copyright © 2016 François Caillet. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class LightsController: UITableViewController {
+
+}
